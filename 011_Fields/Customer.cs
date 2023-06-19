4e97e5cb8c20eb74ba96caf60ca6ba9bfc5a86c4
Jordan Schlak <jaschlak@gmail.com>
2023-06-19 09:19:05 -0500

    added Fields, reinitialization, readonly lesson

---
+++ 011_Fields/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _011_Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+
+        // contstructor initialization
+        public List<Order> Orders = new List<Order>();
+
+        // If readonly, can only be initialized once in the same object, otherwise throws error
+        //public readonly List<Order> Orders = new List<Order>();
+
+        // contstructor initialization
+        public Customer(int id)
+        {
+            this.Id = Id;
+        }
+
+        // Overloaded contstructor initialization
+        public Customer(int id, string Name)
+            : this(id)              // not best pracice
+        {
+            this.Name = Name;
+        }
+
+        public void Promote()
+        {
+            // This is shows reinitialization destroys older Order lists
+            Orders = new List<Order>();
+        }
+    }
+}
---
+++ 011_Fields/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _011_Fields
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(12);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            
+
+            Console.WriteLine("Before re-initialization count is: {0}", customer.Orders.Count());
+            customer.Promote();
+            Console.WriteLine("After re-initialization count is:  {0}", customer.Orders.Count());
+
+            Console.ReadLine();
+        }
+    }
+}
