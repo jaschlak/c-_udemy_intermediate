90f3a19b980b76bf33f884c5f789c7822c5655b1
Jordan Schlak <jaschlak@gmail.com>
2023-06-19 12:12:34 -0500

    012_Access_Modifiers private variable encapsulation

---
+++ 012_Access_Modifiers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _012_Access_Modifiers
+{
+    public class Person
+    {
+        // variable is private, needs getter/setter
+        private DateTime _birthdate;
+
+        // setter
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        // getter
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            // access private variable value with encapsulation
+            person.SetBirthdate(new DateTime(1975, 7, 13));
+            Console.WriteLine("birthday set to {0}", person.GetBirthdate());
+            Console.ReadLine(); 
+        }
+    }
+}
