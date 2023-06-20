9b8643a40fce71966845e3cc138a1b000aa88277
Jordan Schlak <jaschlak@gmail.com>
2023-06-20 07:44:58 -0500

    022_AccessModifiers show AM protected before logic changed

---
+++ 022_AccessModifiers/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _022_AccessModifiers
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var rating = CalculateRating(excludeOrders: true);
+            if (rating == 0)
+            {
+                Console.WriteLine("Promoted to Level 1");
+            }
+            else
+            {
+                Console.WriteLine("Promoted to Level 2");
+            }
+        }
+
+        // when CalculateRating is private, it cannot be seen by Main
+        // when CalcualteRating is protected, it can be seen by Main
+        protected int CalculateRating(bool excludeOrders)
+        {
+            return 0;
+        }
+    }
+}
---
+++ 022_AccessModifiers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _022_AccessModifiers
+{
+    public class GoldCustomer : Customer
+    {
+        public void OfferVoucher()
+        {
+            var rating = this.CalculateRating(excludeOrders: true);
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            var customer = new Customer();
+        }
+    }
+}
