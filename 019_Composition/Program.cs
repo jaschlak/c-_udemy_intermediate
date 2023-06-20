73177d52081dc6cf6bb4fda73c1487084e11b608
Jordan Schlak <jaschlak@gmail.com>
2023-06-20 07:05:08 -0500

    019_Composition

---
+++ 019_Composition/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _019_Composition
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //make new instance for param
+            var dbMigrator = new DbMigrator(new Logger());
+
+            //make new variable for param
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+
+            dbMigrator.Migrate();
+
+            installer.Install();
+
+            Console.ReadLine();
+        }
+    }
+}
