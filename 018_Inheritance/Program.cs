d72cdd8b9b853fba617e2ba532f9067f2b1d4759
Jordan Schlak <jaschlak@gmail.com>
2023-06-20 06:29:26 -0500

    018_Inheritance

---
+++ 018_Inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _018_Inheritance
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+            // Text inherits PresentationObject
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
+            text.Duplicate();
+            text.AddHyperLink("https://www.fridaypage.com");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 018_Inheritance/Text.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _018_Inheritance
+{
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperLink(string url)
+        {
+            Console.WriteLine("We added a linky to {0}", url);
+        }
+    }
+}
